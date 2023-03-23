17ac90faaad52b40b18c030089e046925f42ff22
vicarb <vcardenasb@protonmail.com>
2023-03-23 20:10:26 -0300

    first commit

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-full bg-sky-600 text-white py-4 px-4 sm:px-6 lg:px-8 fixed bottom-0">
+      <div className="max-w-7xl mx-auto">
+        <p className="text-center text-sm">
+          © 2023 My Remote Jobs. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
